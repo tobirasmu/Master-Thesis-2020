2ba6626b34b9c2dfd2a0c4650f792d76ab6620de
Tobias Engelhardt Rasmussen <s153057@student.dtu.dk>
2020-08-13 22:14:18 +0200

    ANN in pytorch

--- Classifiyng MNIST/ANN_CNN_MNIST.py
+++ Classifiyng MNIST/ANN_CNN_MNIST.py
@@ -23,18 +23,35 @@ img_test, labs_test = mndata.load_testing()
 img_train, labs_train = np.array(img_train), np.array(labs_train)
 img_test, labs_test = np.array(img_test), np.array(labs_test)
 
+#%% For being able to plot the handwritten digits. Either one by one, or a matrix of them
 def showImage(img, label=""):
-    plt.imshow(np.reshape(np.array(img),newshape=(28,28)),vmin=0,vmax=255,cmap="gray")
+    plt.imshow(np.reshape(img,newshape=(28,28)),vmin=0,vmax=255,cmap="gray")
     plt.title(label)
+    
+def plotMany(img_L,B=10,H=10):
+    # B is how many pictures on the x-axis, and H is the y-axis
+    nr = 0
+    canvas = np.zeros((1,28*B))
+    for i in range(H):
+        temp = img_L[nr].reshape((28,28))
+        nr+= 1
+        for j in range(B-1):
+            temp = np.concatenate((temp,img_L[nr].reshape((28,28))),axis = 1)
+            nr+= 1
+        canvas = np.concatenate((canvas, temp),axis = 0)
+    plt.imshow(canvas[1:,:],vmin=0,vmax=255,cmap="gray")
+    plt.axis('off')
+
+    #%%
     
 # Example of plotting
 plt.subplot(1,2,1)
 showImage(img_train[201],labs_train[201])
 plt.subplot(1,2,2)
 showImage(img_train[100],labs_train[100])
-
+#%%
 # Some of the pictures are hard to recognize even for humans.
-
+plotMany(img_train,20,30)
 #%% ANN using the pytorch framework
     
     
