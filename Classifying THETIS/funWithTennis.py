7694b90a890aa619fdd895e1a5f3f9cba3713011
Tobias Engelhardt Rasmussen <s153057@student.dtu.dk>
2020-10-28 15:59:53 +0100

    Update

--- Classifying THETIS/funWithTennis.py
+++ Classifying THETIS/funWithTennis.py
@@ -11,6 +11,48 @@ import torch as tc
 import numpy as np
 import os
 from sys import getsizeof
+from tensorly.decomposition import partial_tucker, tucker
+from tensorly.tenalg import multi_mode_dot
+import tensorly as tl
+tl.set_backend('pytorch')
+
+        
+# %% Getting just one video out
+
+path = "/Users/Tobias/Desktop/"
+os.chdir(path)
+if (not os.path.exists('Test')):
+    os.makedirs('Test')
+
+videoFile = "/Users/Tobias/Desktop/Data/VIDEO_RGB/forehand_flat/p6_foreflat_s3.avi"
+cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
+
+frames = []
+while(cap.isOpened()):
+    frameId = cap.get(1)
+    ret, frame = cap.read()
+    if (not ret):
+        break
+    name = 'Test/frame%d_skelet3D.jpg' % frameId
+    frames.append(tc.tensor(frame, dtype=tc.float32))
+p6 = tc.stack(frames)
+
+# decomposing the frame to separate the dynamical information
+core, [frames, W, H, ch] = partial_tucker(p6, modes = [0,1,2,3], ranks = [1, 480, 640, 3])
+
+# %% New video
+new = tl.tucker_to_tensor((core,[frames, W, H, ch]))
+errors = p6-new
+
+out = cv2.VideoWriter('ErrorsVideo.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 18, (640, 480))
+
+for i in range(new.shape[0]):
+    frame = errors[i]
+    print(i, frame.shape)
+    out.write(np.array(frame, dtype= np.uint8))
+out.release()
+
+# %% Making all videos in a directory into tensors
 
 amount = 0
 if (not os.path.exists('Backhand_frames')):
