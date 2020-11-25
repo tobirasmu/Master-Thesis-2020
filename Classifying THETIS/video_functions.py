895ce5519c917844e10400073e8e06bb5fb44bbe
Tobias Engelhardt Rasmussen <s153057@student.dtu.dk>
2020-11-25 11:51:27 +0100

    Update

--- Classifying THETIS/video_functions.py
+++ Classifying THETIS/video_functions.py
@@ -112,7 +112,7 @@ def loadShotType(shot_type, directory, input_file=None, length=None, ignore_inds
 
 
 # %% Writes all names of directory to file
-def write_names2file(directory, out_directory=None):
+def writeNames2file(directory, out_directory=None):
     """
     Write all the file names of a directory to a file in the out_directory.
     """
@@ -128,19 +128,45 @@ def write_names2file(directory, out_directory=None):
 
 
 # %% Writes a tensor to a video
-def write_tensor2video(x, name, out_directory=None):
+def writeTensor2video(x, name, out_directory=None):
     """
-    Writes a tensor of shape (ch, num_frames, height, width) to a video at the given out_directory. If no directory is
-    given, it will just be placed in the given directory with the name.
+    Writes a tensor of shape (ch, num_frames, height, width) or (num_frames, height, width) (for B/W) to a video at the
+    given out_directory. If no directory is given, it will just be placed in the current directory with the name.
     """
+    if x.type() != 'torch.ByteTensor':
+        x = x.type(dtype=tc.uint8)
     if out_directory is None:
         out_directory = os.getcwd() + '/'
     name = name + '.avi'
-    ch, num_frames, height, width = x.shape
-    writer = cv2.VideoWriter(out_directory + name, cv2.VideoWriter_fourcc('M','J','P','G'), 18, (width,height))
+    if len(x.shape) == 3:
+        num_frames, height, width = x.shape
+        ch = 1
+    else:
+        ch, num_frames, height, width = x.shape
+    writer = cv2.VideoWriter(out_directory + name, cv2.VideoWriter_fourcc('M','J','P','G'), FRAME_RATE, (width,height))
     for i in range(num_frames):
         frame = np.moveaxis(x[:, i, :, :].type(dtype=tc.uint8).numpy(), 0, -1)
         if ch == 1:
             frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        else:
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         writer.write(frame)
     writer.release()
+
+
+# %% Showing a frame
+def showFrame(x, title=None):
+    """
+    Takes in a tensor of shape (ch, height, width) or (height, width) (for B/W) and plots the image using
+    matplotlib.pyplot
+    """
+    if x.type() != "torch.ByteTensor":
+        x = x.type(dtype=tc.uint8)
+    if len(x.shape) == 2:
+        plt.imshow(x.numpy(), cmap="gray")
+    else:
+        plt.imshow(np.moveaxis(x.numpy(), 0, -1))
+    if title is not None:
+        plt.title(title)
+    plt.axis('off')
+    plt.show()
