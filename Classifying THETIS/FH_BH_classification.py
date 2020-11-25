e38570daba709737e46ace92ad3b3c5970a5eb7b
Tobias Engelhardt Rasmussen <s153057@student.dtu.dk>
2020-11-25 09:49:39 +0100

    Updates

--- Classifying THETIS/FH_BH_classification.py
+++ Classifying THETIS/FH_BH_classification.py
@@ -8,36 +8,27 @@ Created on Wed Nov 11 11:49:55 2020
 Trying to classify the flat forehands and the backhands only using the depth 
 videos.
 """
-import cv2
-import csv
-import torch as tc
 import os
+path = "/Users/Tobias/Google Drev/UNI/Master-Thesis-Fall-2020/Classifying THETIS"
+os.chdir(path)
+import cv2
+from time import process_time
 import matplotlib.pyplot as plt
+import torch as tc
+
+
 import torch.nn as nn
-import numpy as np
 from torch.nn import Linear, Conv2d, BatchNorm2d, AvgPool2d, MaxPool2d, Dropout2d, Dropout, BatchNorm1d
 import tensorly as tl
 from tensorly.decomposition import partial_tucker
 from tensorly.tenalg import multi_mode_dot, mode_dot
-from time import process_time
+
+from video_functions import loadShotType, write_names2file
 
 tl.set_backend('pytorch')
-path = "/Users/Tobias/Google Drev/UNI/Master-Thesis-Fall-2020/Classifying THETIS"
-os.chdir(path)
 
-# %% Loading the filenames and making file with them
-"""
-To read and write the names of the videos into a file in order to manually
-allocate the time for the middle of the stroke.
-"""
-path = "/Users/Tobias/Desktop/"
-os.chdir(path)
-directory = "Data/VIDEO_Depth/forehand_flat/"
-files = sorted(os.listdir(directory))
-file = open('backhand_filenames.csv', 'w')
-writer = csv.writer(file)
-for filename in files: writer.writerow([filename])
-file.close()
+
+# %%
 
 """ 
 FOREHANDS
@@ -59,113 +50,20 @@ The very first seems to be wrong.
     p1_foreflat_depth_s1.avi (0) (RGB is wrong - is actually p50)
 """
 
-
 # %% The loading functions
 FRAME_RATE = 18
 LENGTH = 1.5
-def loadVideo(filename, middle= None, blackwhite= True):
-    """
-    Loads a video and returns a 4D tensor (frame, channel, height, width).
-    """
-    cap = cv2.VideoCapture(filename)
-    if (middle == None):
-        numFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        firstFrame = 0
-        lastFrame = numFrames
-    else:
-        numFrames = int(LENGTH * FRAME_RATE + 1)
-        firstFrame = int((middle - LENGTH / 2) * FRAME_RATE)
-        lastFrame = int(firstFrame + LENGTH * FRAME_RATE)
-    ch = 1 if blackwhite else 3
-    height, width = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frames = tc.empty((ch,numFrames, height, width))
-    framesLoaded = 0
-    framesRead = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret == False or framesRead == lastFrame:
-            break
-        framesRead += 1
-        if framesRead >= firstFrame:
-            if blackwhite:
-                frame = np.expand_dims(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 0)
-            else:
-                frame = np.moveaxis(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB),-1,0)
-            frames[:,framesLoaded,:,:] = tc.tensor(frame)
-            framesLoaded += 1
-    cap.release()
-    return frames
-
-def loadShotType(shotType, directory, inputFile= None, dontLoadInds= None):
-    """
-    Loads all the videos of a directory and makes them into a big tensor. If the inputfile is given, the output will be
-    a big tensor of shape (numVideos, channels, numFrames, height, width), otherwise the output will be a list of 4D
-    tensors with different number of Frames.
-    Inputfile contains the middle of the shot (time), dontLoadsInds are the videos that will not be loaded, due to
-    potential problems.
-    Shot types :
-     - 0  Forehand flat
-     - 1  Backhand
-    Output:
-        Tensor of dimension (numVideos, channels, numFrames, height, width) where the 3 first channels correspond to the
-        RGB video, while the last channel is the black/white depth video.
-    """
-    shotTypes = {
-        0: "forehand_flat/",
-        1: "backhand/"
-    }
-    directoryRGB = directory + "VIDEO_RGB/" + shotTypes.get(shotType)
-    directoryDep = directory + "VIDEO_Depth/" + shotTypes.get(shotType)
-    filenamesRGB = sorted(os.listdir(directoryRGB))
-    filenamesDep = sorted(os.listdir(directoryDep))
-
-    if inputFile == None:
-        if dontLoadInds != None:
-            if (len(dontLoadInds) > 1):
-                dontLoadInds = sorted(dontLoadInds, reverse= True)
-            for ind in dontLoadInds:
-                filenamesRGB.pop(ind)
-                filenamesDep.pop(ind)
-        allVideos = []
-        for i in range(len(filenamesRGB)):
-            thisRGB = loadVideo(directoryRGB + filenamesRGB[i], blackwhite= False)
-            thisDep = loadVideo(directoryDep + filenamesDep[i], blackwhite= True)
-            allVideos.append(tc.cat((thisRGB,thisDep),0))
-        return allVideos
-    else:
-        rFile = open(inputFile, "r")
-        reader = csv.reader(rFile, delimiter= ";")
-        files = list(reader);
-        if dontLoadInds != None:
-            if (len(dontLoadInds) > 1):
-                dontLoadInds = sorted(dontLoadInds, reverse= True)
-            for ind in dontLoadInds:
-                files.pop(ind)
-                filenamesRGB.pop(ind)
-                filenamesDep.pop(ind)
-        numVideos = len(filenamesRGB)
-        thisRGB = loadVideo(directoryRGB + filenamesRGB[0], float(files[0][1]), blackwhite= False)
-        thisDep = loadVideo(directoryDep + filenamesDep[0], float(files[0][1]), blackwhite= True)
-        thisVideo = tc.cat((thisRGB, thisDep),0)
-        allVideos = tc.empty((numVideos, *thisVideo.shape))
-        allVideos[0] = thisVideo
-        for i in range(1,numVideos):
-            middle = float(files[i][1])
-            thisRGB = loadVideo(directoryRGB + filenamesRGB[i], middle= middle, blackwhite= False)
-            thisDep = loadVideo(directoryDep + filenamesDep[i], middle= middle, blackwhite= True)
-            allVideos[i] = tc.cat((thisRGB, thisDep), 0)
-        return allVideos
 
 # %% Loading the videos
 t = process_time()
 directory = "/Users/Tobias/Desktop/Data/"
 # Forehands
 inputForehand = "/Users/Tobias/Google Drev/UNI/Master-Thesis-Fall-2020/Classifying THETIS/forehand_filenames_adapted.csv"
-forehands = loadShotType(0, directory, inputFile= inputForehand)
+forehands = loadShotType(0, directory, input_file=inputForehand, length=1.5)
 # Backhand
 inputBackhand = "/Users/Tobias/Google Drev/UNI/Master-Thesis-Fall-2020/Classifying THETIS/backhand_filenames_adapted.csv"
-backhands = loadShotType(1, directory, inputFile= inputBackhand)
-print("Time to load: ",process_time()-t)
+backhands = loadShotType(1, directory, input_file=inputBackhand, length=1.5)
+print("Time to load: ", process_time() - t)
 
 # %% Compile into one large dataset.
 tc.manual_seed(42)
@@ -233,3 +131,5 @@ for i in range(forehands.shape[0]):
         frame = cv2.cvtColor(frame.numpy(), cv2.COLOR_GRAY2RGB)
         out.write(frame)
 out.release()
+import numpy as np
+
---
+++ Classifying THETIS/video_functions.py
@@ -0,0 +1,142 @@
+import numpy as np
+import torch as tc
+import cv2
+import csv
+import os
+
+FRAME_RATE = 18
+
+
+# %% Function for loading a single video
+def loadVideo(filename, middle=None, length=None, b_w=True):
+    """
+    Loads a video using the full path and returns a 4D tensor (channels, frames, height, width).
+    INPUT:
+        filename - the full path of the video
+        middle - the time at the point of the middle of the shot in the video
+        length - the desired length to take out in seconds. Half this time will be included on each side of the middle
+        b_w    - true if a black/white video is wanted (only one channel)
+    Output:
+        A tensor of dimension (channels, frames, height, width) where the number of frames = length * 18 + 1
+    """
+    cap = cv2.VideoCapture(filename)
+    if middle is None:
+        numFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        firstFrame = 0
+        lastFrame = numFrames
+    else:
+        numFrames = int(length * FRAME_RATE + 1)
+        firstFrame = int((middle - length / 2) * FRAME_RATE)
+        lastFrame = int(firstFrame + length * FRAME_RATE)
+    ch = 1 if b_w else 3
+    height, width = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frames = tc.empty((ch, numFrames, height, width))
+    framesLoaded = 0
+    framesRead = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret is False or framesRead is lastFrame:
+            break
+        framesRead += 1
+        if framesRead >= firstFrame:
+            if b_w:
+                frame = np.expand_dims(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 0)
+            else:
+                frame = np.moveaxis(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), -1, 0)
+            frames[:, framesLoaded, :, :] = tc.tensor(frame)
+            framesLoaded += 1
+    cap.release()
+    return frames
+
+
+# %% Function for loading an entire directory
+def loadShotType(shot_type, directory, input_file=None, length=None, ignore_inds=None):
+    """
+    Loads all the videos of a directory and makes them into a big tensor. If the inputfile is given, the output will be
+    a big tensor of shape (numVideos, channels, numFrames, height, width), otherwise the output will be a list of 4D
+    tensors with different number of Frames.
+    Inputfile contains the middle of the shot (time), dontLoadsInds are the videos that will not be loaded, due to
+    potential problems.
+    Shot types :
+     - 0  Forehand flat
+     - 1  Backhand
+    Output:
+        Tensor of dimension (numVideos, channels, numFrames, height, width) where the 3 first channels correspond to the
+        RGB video, while the last channel is the black/white depth video.
+    """
+    shot_types = {
+        0: "forehand_flat/",
+        1: "backhand/"
+    }
+    directory_rgb = directory + "VIDEO_RGB/" + shot_types.get(shot_type)
+    directory_dep = directory + "VIDEO_Depth/" + shot_types.get(shot_type)
+    filenames_rgb = sorted(os.listdir(directory_rgb))
+    filenames_dep = sorted(os.listdir(directory_dep))
+
+    if input_file is None:
+        if ignore_inds is not None:
+            if len(ignore_inds) > 1:
+                ignore_inds = sorted(ignore_inds, reverse=True)
+            for ind in ignore_inds:
+                filenames_rgb.pop(ind)
+                filenames_dep.pop(ind)
+        all_videos = []
+        for i in range(len(filenames_rgb)):
+            thisRGB = loadVideo(directory_rgb + filenames_rgb[i], b_w=False)
+            thisDep = loadVideo(directory_dep + filenames_dep[i], b_w=True)
+            all_videos.append(tc.cat((thisRGB, thisDep), 0))
+        return all_videos
+    else:
+        rFile = open(input_file, "r")
+        reader = csv.reader(rFile, delimiter=";")
+        files = list(reader)
+        if ignore_inds is not None:
+            if len(ignore_inds) > 1:
+                ignore_inds = sorted(ignore_inds, reverse=True)
+            for ind in ignore_inds:
+                files.pop(ind)
+                filenames_rgb.pop(ind)
+                filenames_dep.pop(ind)
+        numVideos = len(filenames_rgb)
+        thisRGB = loadVideo(directory_rgb + filenames_rgb[0], float(files[0][1]), length=length, b_w=False)
+        thisDep = loadVideo(directory_dep + filenames_dep[0], float(files[0][1]), length=length, b_w=True)
+        thisVideo = tc.cat((thisRGB, thisDep), 0)
+        all_videos = tc.empty((numVideos, *thisVideo.shape))
+        all_videos[0] = thisVideo
+        for i in range(1, numVideos):
+            middle = float(files[i][1])
+            thisRGB = loadVideo(directory_rgb + filenames_rgb[i], middle=middle, length=length, b_w=False)
+            thisDep = loadVideo(directory_dep + filenames_dep[i], middle=middle, length=length, b_w=True)
+            all_videos[i] = tc.cat((thisRGB, thisDep), 0)
+        return all_videos
+
+
+# %% Writes all names of directory to file
+def write_names2file(directory, out_directory=None):
+    """
+    Write all the file names of a directory to a file in the out_directory.
+    """
+    if out_directory is None:
+        out_directory = directory
+    out_name = out_directory + str.split(directory, '/')[-2] + '_filenames.csv'
+    file = open(out_name, 'w')
+    writer = csv.writer(file)
+    files = sorted(os.listdir(directory))
+    for filename in files:
+        writer.writerow([filename])
+    file.close()
+
+
+# %% Writes a tensor to a video
+def write_tensor2video(X, name, out_directory=None):
+    if out_directory is None:
+        out_directory = os.getcwd() + '/'
+    name = name + '.avi'
+    ch, num_frames, height, width = X.shape
+    writer = cv2.VideoWriter(out_directory + name, cv2.VideoWriter_fourcc('M','J','P','G'), FRAME_RATE, (width,height))
+    for i in range(num_frames):
+        frame = np.moveaxis(X[:,i,:,:].type(dtype=tc.uint8).numpy(),0,-1)
+        if (ch is 1):
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        writer.write(frame)
+    writer.release()
